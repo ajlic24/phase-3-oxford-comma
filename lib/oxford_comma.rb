f8ea2f35d0f5529f53fa781440179ff9a3e51047
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-16 18:48:56 -0500

    commit tree 3e0718a3f2fd3f97c41ca6596eb77ddb7f397d18

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array [0] 
+    elsif array.length == 2 
+        a = array.pop
+        array.push('and', a).join(' ')
+    else 
+        a = array.pop
+        array.map {|word| word + ','}.push('and', a).join(' ')
+    end
 end
+
+# pp oxford_comma(["kiwi", "durian", "something"])
